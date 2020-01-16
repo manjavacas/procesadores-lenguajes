862cb0256a3a8247ecf3b6f596152187e13c3291
manjavacas <Antonio.Manjavacas@alu.uclm.es>
2020-01-16 08:55:51 +0100

    [lnx] parse.sh fixed

--- parse.sh
+++ parse.sh
@@ -1,4 +1,4 @@
 export CUP_RUNTIME="./lib/java-cup-11b-runtime.jar"
 export BUILD_DIR="./build/"
 
-java -cp $(CLASSPATH):$(CUP_RUNTIME):$(BUILD_DIR) gachaneitor.parser $@
+java -cp "$CLASSPATH:$CUP_RUNTIME:$BUILD_DIR" gachaneitor.parser $@
